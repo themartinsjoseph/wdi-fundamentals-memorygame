1c7319030d0e795ab09115b240a50faa8342b35b
Joe Martin <jmartin@PRRs-MacBook-Pro.local>
2017-01-23 19:18:08 -0800

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,9 @@
 console.log("JS file is connected to HTML! Woo!")
+/* I'm not sure what I'm supposed to comment, but you asked that I comment */
+
+var cardOne = "queen"; 
+var cardTwo = "queen"; 
+var cardThree = "king";
+var cardFour = "king";
+
+
