2db08e1e13086180547f79e71e63d0a5c74da1c2
Joe Martin <jmartin@PRRs-MacBook-Pro.local>
2017-01-23 23:29:35 -0800

    added in logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,4 +6,31 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+if (cardFour === cardTwo) {
+
+alert('Sorry, try again.'); 
+
+} else if (cardOne === cardTwo) {
+
+alert('You found a match!')
+
+} else if (cardThree === cardFour) {
+
+alert('You found a match!')
+
+} else if (cardThree === cardOne) {
+
+alert('Sorry, try again.')
+
+} else if (cardThree === cardTwo) {
+
+alert('Sorry, try again.')
+
+} else if (cardFour === cardOne) {
+
+alert('Sorry, try again.')
+
+} 
+
+
 
