3bf3d165a0420d308bfe00e334d314eb0c6a9c02
Joe Martin <jmartin@PRRs-MacBook-Pro.local>
2017-01-27 09:06:30 -0800

    unit 11 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,50 +1,58 @@
 console.log("JS file is connected to HTML! Woo!")
 
-// var cardOne = 'queen'; 
-// var cardTwo = 'queen'; 
-// var cardThree = 'king';
-// var cardFour = 'king';
-
+// var
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 var gameBoard = document.getElementById('game-board'); 
 
+//functions 
 function createCards() {
-	for (var i=0; i<4; i++) {
+	for (var i=0; i<cards.length; i++) {
 	var cardElement = document.createElement('div');
 	cardElement.className = 'card';
 	gameBoard.appendChild(cardElement);
+	cardElement.setAttribute('data-card', cards[i]);
+	cardElement.addEventListener('click', isTwoCards);
+
+
+    gameBoard.appendChild(cardElement);
+    gameBoard.appendChild(cardElement);
+
+}
+
+}
+
+function isTwoCards() {
+cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+    if (this.getAttribute('data-card') === 'king') {
+        this.innerHTML = "<img src="kinghearts.png" alt="King of Hearts">"; // king
+    } else {
+        this.innerHTML = "<img src="queenhearts.png" alt="Queen of Hearts">"; // queen
+    }
+
+if (cardsInPlay.length === 2) {
+   isMatch(cardsInPlay);
+   cardsInPlay = [] ;
 }
 
 }
 
-
-
-
-
-/*if (cardFour === cardTwo) {
-
-alert('Sorry, try again.'); 
-
-} else if (cardOne === cardTwo) {
-
-alert('You found a match!')
-
-} else if (cardThree === cardFour) {
-
-alert('You found a match!')
-
-} else if (cardThree === cardOne) {
-
-alert('Sorry, try again.')
-
-} else if (cardThree === cardTwo) {
-
-alert('Sorry, try again.')
-
-} else if (cardFour === cardOne) {
-
-alert('Sorry, try again.')
-
+function isMatch () {
+if (cards[0] === cards[1]) {
+   alert('You found a match!');
+  } else if (cards[2] === cards[3]) {
+      alert('You found a match!');
+     } else if {
+       alert('Sorry, try again.');
+  } 
+  console.log(createCards);
 } 
-*/
+
+}
+
+
+
+
 
 
